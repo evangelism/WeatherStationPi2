dc3b74cb6ac7783440da3bbec9ed1b57db27714a
Dmitri Soshnikov <dmitri@soshnikov.com>
2015-09-08 00:50:34 +0300

    Initial Commit. Project structured to have MakerFriendly lib

---
+++ WeatherStationPi2/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using MakerFriendly.I2C;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace WeatherStationPi2
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        BMP180 sensor = new BMP180();
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await sensor.Init();
+
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            timer.Tick += (sender, o) =>
+            {
+                var temp = sensor.Temperature;
+                Debug.WriteLine("Temperature = " + temp);
+                TheTextBlock.Text = $"Temperature : {temp}°C";
+            };
+            timer.Start();
+        }
+
+
+    }
+}
